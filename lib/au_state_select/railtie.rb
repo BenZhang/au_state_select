fdfd3c675328c3d0bb256d2f23fbbe15e83aec42
Ben Zhang <bzbnhang@gmail.com>
2015-01-08 11:16:35 +1030

    tweak railtie

--- lib/au_state_select/railtie.rb
+++ lib/au_state_select/railtie.rb
@@ -3,15 +3,17 @@ require 'rails/railtie'
 module AUStateSelect
   class Railtie < Rails::Railtie
     initializer "au_state_select.configure_rails_initialization" do
-      SimpleForm.input_mappings = { /state/ => :state }
 
+      SimpleForm.input_mappings = { /state/ => :state }
       module ::SimpleForm
         mattr_accessor :state_priority
         @@state_priority = nil
+        
         class FormBuilder < ActionView::Helpers::FormBuilder
           map_type :state, to: SimpleForm::Inputs::PriorityInput
         end
       end
+
     end
   end
 end
