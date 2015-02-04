5c165db6dde3a398456749da354424d92b78138d
Ben Zhang <bzbnhang@gmail.com>
2015-02-04 12:34:45 +1030

    1. fix prompt bug 2. change variable name

--- lib/au_state_select/instance_tag.rb
+++ lib/au_state_select/instance_tag.rb
@@ -13,23 +13,27 @@ module AUStateSelect
     def state_select(priority_states, options, html_options)
       selected = object.send(@method_name) if object.respond_to?(@method_name)
 
-      countries = ""
+      states = ""
 
       if options.present? and (options[:include_blank] or (options[:prompt] and !selected))
-        option = options[:include_blank] == true ? "" : options[:include_blank]
-        countries += "<option>#{option}</option>\n"
+        if options[:include_blank].present?
+          option = options[:include_blank] == true ? "" : options[:include_blank]
+        elsif options[:prompt].present?
+          option = options[:prompt] == true ? "Please Select" : options[:prompt]
+        end
+        states += "<option>#{option}</option>\n"
       end
 
       if priority_states
-        countries += options_for_select(priority_states, selected)
-        countries += "<option value=\"\" disabled=\"disabled\">-------------</option>\n"
+        states += options_for_select(priority_states, selected)
+        states += "<option value=\"\" disabled=\"disabled\">-------------</option>\n"
       end
-      countries = countries + options_for_select([['Australian Capital Territory', 'ACT'],['New South Wales', 'NSW'],['Northern Territory', 'NT'],['Queensland', 'QLD'],['South Australia', 'SA'],['Tasmania', 'TAS'],['Victoria', 'VIC'],['Western Australia', 'WA']], selected)
+      states = states + options_for_select([['Australian Capital Territory', 'ACT'],['New South Wales', 'NSW'],['Northern Territory', 'NT'],['Queensland', 'QLD'],['South Australia', 'SA'],['Tasmania', 'TAS'],['Victoria', 'VIC'],['Western Australia', 'WA']], selected)
 
       html_options = html_options.stringify_keys
       add_default_name_and_id(html_options)
 
-      content_tag(:select, countries.html_safe, html_options)
+      content_tag(:select, states.html_safe, html_options)
     end
   end
 
