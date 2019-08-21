6f0f99e0f0760a2fe2589d37755e2ebc8d68b7b2
Ben Zhang <bzbnhang@gmail.com>
2019-08-21 10:08:49 +0930

    bug fix

--- lib/au_state_select/form_builder.rb
+++ lib/au_state_select/form_builder.rb
@@ -1,7 +1,14 @@
 module AUStateSelect
   module FormBuilder
-    def state_select(method, priority_countries = nil, options = {}, html_options = {})
-      @template.state_select(@object_name, method, priority_countries, options.merge(object: @object), html_options)
+    def state_select(method, priority_state = nil, options = {}, html_options = {})
+      if Hash === priority_or_options
+        html_options = options
+        options = priority_or_options
+      else
+        options[:priority_state] = priority_or_options
+      end
+
+      @template.country_select(@object_name, method, objectify_options(options), @default_options.merge(html_options))
     end
   end
 end
